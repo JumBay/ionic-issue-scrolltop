6adcd6a2e75790cd5505c1ac78c94c702003bc47
Jean-Baptiste Malatrasi <jb.malatrasi@gmail.com>
2019-10-23 23:38:21 +0200

    Issue https://github.com/ionic-team/ionic/issues/19678

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { SamplePage } from './sample.page';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([
+      { path: '', component: Tab1Page },
+      { path: 'sample', component: SamplePage }
+    ])
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, SamplePage]
 })
 export class Tab1PageModule {}
